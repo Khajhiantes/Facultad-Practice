fc025e293c404dc0a46abcacd7fb726c8f492c0b
Martin <mkzamora02@gmail.com>
2024-05-13 00:00:49 -0300

    that is my final version

---
+++ Clases-Activity/Clases-Activity/Dice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Activity
+{
+    public class Dice
+    {
+        private static Random Random = new Random();
+
+        public void bet(int number)
+        {
+            int randomN= Random.Next(1, 7);
+            if (number == randomN)
+            {
+                Console.WriteLine("You win");
+                Console.WriteLine($"You rolled a {number} " );
+                Console.WriteLine($"The dice rolled a {randomN}");
+            }
+            else
+            {
+                Console.WriteLine("You lose");
+                Console.WriteLine($"You rolled a {number}" );
+                Console.WriteLine($"The dice rolled a {randomN}" );
+            }
+        }
+    }
+}
--- Clases-Activity/Clases-Activity/Program.cs
+++ Clases-Activity/Clases-Activity/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Clases_Activity;
+
+Console.WriteLine("choice a number to bet between 1 and 6 ");
+int number = int.Parse(Console.ReadLine());
+while (number>6 || number<1)
+{
+    Console.WriteLine("choice a number to bet between 1 and 6! pleaseeee!!!! ");
+    number = int.Parse(Console.ReadLine());
+}
+
+
+Dice dice = new Dice();
+dice.bet(number);
+
