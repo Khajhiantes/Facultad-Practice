03023b7b0163fc9789cd843e16995032fb0f6fd2
Martin <mkzamora02@gmail.com>
2024-05-23 16:20:04 -0300

    OOP

---
+++ Zoo-Animals/Zoo-Animals/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo_Animals
+{
+    internal class Animal
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal speaks");
+        }
+    }
+}
---
+++ Zoo-Animals/Zoo-Animals/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Zoo_Animals;
+
+List<Animal> animals = new List<Animal>
+{
+    new Bird{Name = "Tweety"},
+    new Elephant{Name = "Dumbo"},
+    new Lion{Name = "Simba"}
+};
+foreach (var animal in animals)
+{
+    Console.WriteLine($"{animal.Name} says:");
+    animal.Speak();
+}
